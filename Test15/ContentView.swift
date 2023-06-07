5370a6747537aa8aea239b9d938c4586f9b8e391
hoshi005 <hoshi005@me.com>
2023-06-07 20:24:57 +0900

    Initial Commit

---
+++ Test15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test15
+//
+//  Created by Susumu Hoshikawa on 2023/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test15/Test15App.swift
@@ -0,0 +1,17 @@
+//
+//  Test15App.swift
+//  Test15
+//
+//  Created by Susumu Hoshikawa on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct Test15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
