2f7c36a4078d1d17bc30b20745ddd8e99958e725
hoshi005 <hoshi005@me.com>
2023-06-11 11:44:57 +0900

    スクロール位置を取得・設定する方法

--- Test15/ScrollNewAPI.swift
+++ Test15/ScrollNewAPI.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ScrollNewAPI: View {
     
+    @State private var scrollPosition: Color?
+    
     let colors: [Color] = [.red, .blue, .green, .yellow, .purple]
     
     var body: some View {
@@ -27,6 +29,19 @@ struct ScrollNewAPI: View {
                 .scrollTargetLayout() // NEW: ScrollViewのメインとなるViewにのみ宣言する.
             }
             .scrollTargetBehavior(.paging) // NEW: 画面の幅や高さに合わせてページングする.HStackのspacingは不要.
+            .scrollPosition(id: $scrollPosition) // NEW: スクロール位置を保持可能.
+            
+            // スクロール位置を操作する.
+            Button("scroll to yellow") {
+                withAnimation(.snappy) {
+                    scrollPosition = .yellow
+                }
+            }
+            
+            // スクロール位置を表示する
+            if let scrollPosition {
+                Text(scrollPosition == .red ? "Red" : "Other")
+            }
             
             // Viewサイズに合わせてのページング.
             ScrollView(.horizontal) {
